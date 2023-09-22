f1e71b82012fef291e5b33d65c4ced9527fd16c6
octopengj <octopengj@gmail.com>
2023-09-22 10:29:57 +0900

    Todo separate List

---
+++ INFLEARN/ReactAtoZ/todo/src/components/Lists.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
+import List from "./List";
+
+export default function Lists({ todoData, setTodoData }) {
+  const handleEnd = (result) => {
+    if (!result.destination) return;
+    const newTodoData = [...todoData];
+
+    // 1. 변경시키는 아이템을 배열에서 지워줌
+    // 2. return 값으로 지워진 아이템을 잡아줌
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    // 원하는 자리에 reorderItem을 insert 해줌
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
