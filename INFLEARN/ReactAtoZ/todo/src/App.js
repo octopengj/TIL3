17dc9cfe74da5ead5ab2b558e4de4125d2f3435d
octopengj <octopengj@gmail.com>
2023-09-22 12:09:53 +0900

    Todo useCallback

--- INFLEARN/ReactAtoZ/todo/src/App.js
+++ INFLEARN/ReactAtoZ/todo/src/App.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
 import "./App.css";
+import React, { useState, useCallback } from "react";
 import Lists from "./components/Lists";
 import Form from "./components/Form";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
+
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+    },
+    [todoData]
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,7 +35,11 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
 
-        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Lists
+          handleClick={handleClick}
+          todoData={todoData}
+          setTodoData={setTodoData}
+        />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
--- INFLEARN/ReactAtoZ/todo/src/components/Lists.js
+++ INFLEARN/ReactAtoZ/todo/src/components/Lists.js
@@ -4,7 +4,7 @@ import { Droppable } from "react-beautiful-dnd";
 import { Draggable } from "react-beautiful-dnd";
 import List from "./List";
 
-const Lists = React.memo(({ todoData, setTodoData }) => {
+const Lists = React.memo(({ todoData, setTodoData, handleClick }) => {
   const handleEnd = (result) => {
     if (!result.destination) return;
     const newTodoData = [...todoData];
@@ -32,6 +32,7 @@ const Lists = React.memo(({ todoData, setTodoData }) => {
                 >
                   {(provided, snapshot) => (
                     <List
+                      handleClick={handleClick}
                       key={data.id}
                       id={data.id}
                       title={data.title}
